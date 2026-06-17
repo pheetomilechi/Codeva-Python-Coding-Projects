931151dd0e573f09d036059854ad10dc523365d7
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-18 00:54:47 +0100

    Added another function for marking the items done

--- Todo_list.py
+++ Todo_list.py
@@ -24,3 +24,25 @@ def list_items():
         print(lines)
         for i, line in enumerate(lines):
             print(f"{i+1}, {line.strip()}")
+    
+    def mark_item_done(item_number):
+        with open(FILENAME, "r") as file:
+            lines = file.readlines()
+            
+        if not lines:
+            print("Invalid item number.")
+        elif item_number < 1 or item_number > len(lines):
+            print("Invalid item number")
+            
+            item = lines[item_number-1].rstrip()
+            item = add_strikethrough(item)
+            with open(FILENAME, "w") as file:
+                for i, line in enumerate(lines):
+                    if i == item_number-1:
+                        file.write(f"{item}\n")
+                        print(f"Marked item as done: {item}")
+                    else:
+                        print(line)
+                        file.write(line)
+            
+    
