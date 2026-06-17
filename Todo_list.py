c6152335c377b3f882cbc9b518912daa84a82965
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-18 00:42:36 +0100

    declared the function to add an item and list already added items

--- Todo_list.py
+++ Todo_list.py
@@ -2,5 +2,25 @@ import argparse
 
 FILENAME ="todo.txt"
 
+def add_strikethrough(item):
+    return f"\xlb[9m{item}\x1b[0m"
 
-task = []
+def add(item):
+    with open(FILENAME, "a") as file:
+        file.write(f"{item}\n")
+        file.close()
+        print(f"Added item: {item}")
+        
+def list_items():
+    lines = None
+    try:
+        with open(FILENAME, "r") as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print(f"Error: File '{FILENAME}' not found")
+    if not lines:
+        print("No items found")
+    else:
+        print(lines)
+        for i, line in enumerate(lines):
+            print(f"{i+1}, {line.strip()}")
